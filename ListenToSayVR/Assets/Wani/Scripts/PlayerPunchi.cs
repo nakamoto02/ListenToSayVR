f39f21eb48abbd5823b6a60c7f2118f66917dacb
Nakamoto <nkc.motoki.nakamura@gmail.com>
2017-05-31 18:37:53 +0900

    手(仮)追加

---
+++ ListenToSayVR/Assets/Wani/Scripts/PlayerPunchi.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPunchi : MonoBehaviour
+{
+    AudioSource audio;
+
+	void Start ()
+    {
+        //AudioSourceを取得
+        audio = GetComponent<AudioSource>();
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Enemy")
+        {
+            //音再生
+            audio.Play();
+        }
+    }
+}
