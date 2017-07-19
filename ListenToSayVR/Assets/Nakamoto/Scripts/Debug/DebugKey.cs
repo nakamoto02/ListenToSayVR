a7dd433556cfd5d1f07e0f786ba4477be76e6e6d
Nakamoto <nkc.motoki.nakamura@gmail.com>
2017-07-19 19:03:11 +0900

    DebugKey.csを追加

---
+++ ListenToSayVR/Assets/Nakamoto/Scripts/Debug/DebugKey.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugKey : MonoBehaviour
+{
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+            SceneReset();
+	}
+
+    void SceneReset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
