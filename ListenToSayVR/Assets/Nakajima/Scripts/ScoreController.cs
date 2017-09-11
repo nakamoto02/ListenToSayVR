f9f91aeee54e3ac97f9aa8d12ca650d14ff05ed7
なかジー <nkc.kazuya.nakajima@gmail.com>
2017-09-11 17:58:28 +0900

    タイム、スコア

---
+++ ListenToSayVR/Assets/Nakajima/Scripts/ScoreController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    // スコア
+    int score;
+
+    // スコア表示
+    public Text ScoreLabel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            ScorePluss();
+
+        ScoreLabel.text = ("Score : " + score + "点");
+	}
+
+    public void ScorePluss()
+    {
+        score++;
+    }
+}
---
+++ ListenToSayVR/Assets/Nakajima/Scripts/TimeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeController : MonoBehaviour
+{
+    // 制限時間
+    float Timelimit = 30;
+
+    // 時間表示
+    public Text TimeLabel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Timelimit -= Time.deltaTime;
+
+        TimeLabel.text = ("Time : " + (int)Timelimit + "秒");
+	}
+}
